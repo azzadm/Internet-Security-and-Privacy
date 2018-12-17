8ba3a41514295f71e057bce5ab0f1c27527c1072
Azad M <26673078+azzadm@users.noreply.github.com>
2018-12-17 12:58:30 +0100

    Completed handshake and verification

--- VPN/ForwardClient.java
+++ VPN/ForwardClient.java
@@ -1,10 +1,22 @@
 /**
  * Port forwarding client. Forward data
- * between two TCP ports. Based on Nakov TCP Socket Forward Server 
+ * between two TCP ports. Based on Nakov TCP Socket Forward Server
  * and adapted for IK2206.
- *
+ * <p>
  * See original copyright notice below.
  * (c) 2018 Peter Sjodin, KTH
+ * <p>
+ * Nakov TCP Socket Forward Server - freeware
+ * Version 1.0 - March, 2002
+ * (c) 2001 by Svetlin Nakov - http://www.nakov.com
+ * <p>
+ * Nakov TCP Socket Forward Server - freeware
+ * Version 1.0 - March, 2002
+ * (c) 2001 by Svetlin Nakov - http://www.nakov.com
+ * <p>
+ * Nakov TCP Socket Forward Server - freeware
+ * Version 1.0 - March, 2002
+ * (c) 2001 by Svetlin Nakov - http://www.nakov.com
  */
 
 /**
@@ -13,20 +25,22 @@
  * (c) 2001 by Svetlin Nakov - http://www.nakov.com
  */
 
- 
-import java.lang.AssertionError;
-import java.lang.IllegalArgumentException;
-import java.lang.Integer;
-import java.util.ArrayList;
+
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.io.IOException;
-import java.io.FileInputStream;
- 
-public class ForwardClient
-{
+import java.security.*;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.Base64;
+
+public class ForwardClient {
     private static final boolean ENABLE_LOGGING = true;
     public static final int DEFAULTSERVERPORT = 2206;
     public static final String DEFAULTSERVERHOST = "localhost";
@@ -35,30 +49,86 @@ public class ForwardClient
     private static Arguments arguments;
     private static int serverPort;
     private static String serverHost;
+    private static CertificateFactory certificateFactory;
 
-    private static void doHandshake() throws IOException {
+
+    // Gets the X509 certificate from the input parameter
+    private static X509Certificate getCertificate(String input) throws IOException, CertificateException {
+        certificateFactory = CertificateFactory.getInstance("X.509");
+        FileInputStream certInput = new FileInputStream(input);
+        X509Certificate certificate = (X509Certificate) certificateFactory.generateCertificate(certInput);
+        certInput.close();
+        return certificate;
+    }
+
+    public static X509Certificate generateCertificate(byte[] preCert) throws CertificateException {
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(preCert);
+        CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+        return (X509Certificate) certificateFactory.generateCertificate(inputStream);
+    }
+
+    // Encodes the certificate to send to the server
+    private static String encodeCertificate(X509Certificate certificate) throws CertificateEncodingException {
+        //certificate.getEncoded();
+        byte[] encodedCert = Base64.getEncoder().encode(certificate.getEncoded());
+        return new String(encodedCert);
+    }
+
+    private static X509Certificate decodeCertificate(String encodedCert) throws CertificateException {
+        byte[] decodedCert = Base64.getDecoder().decode(encodedCert.getBytes());
+        return generateCertificate(decodedCert);
+    }
+
+
+    private static void verifyCertificate(X509Certificate caCert, X509Certificate clientCert) throws NoSuchProviderException, CertificateException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+        PublicKey publicKey = caCert.getPublicKey();
+        clientCert.verify(publicKey);
+    }
+
+
+    private static void doHandshake() throws IOException, CertificateException, NoSuchAlgorithmException, NoSuchProviderException, InvalidKeyException, SignatureException {
 
         /* Connect to forward server server */
-        System.out.println("Connect to " +  arguments.get("handshakehost") + ":" + Integer.parseInt(arguments.get("handshakeport")));
+        System.out.println("Connect to " + arguments.get("handshakehost") + ":" + Integer.parseInt(arguments.get("handshakeport")));
         Socket socket = new Socket(arguments.get("handshakehost"), Integer.parseInt(arguments.get("handshakeport")));
 
         /* This is where the handshake should take place */
+        HandshakeMessage toServer = new HandshakeMessage();
 
+        // Extract clientCert from the input parameter, and call method getCertificate
+        X509Certificate cert = getCertificate("C:/Users/azadm/IdeaProjects/VPN_Project/client.pem");
+
+        toServer.putParameter("MessageType", "ClientHello");
+        toServer.putParameter("Certificate", encodeCertificate(cert));
+        toServer.send(socket);
+
+        HandshakeMessage fromServer = new HandshakeMessage();
+
+        System.out.println("Waiting for incoming data from server...");
+        fromServer.recv(socket);
+        if(fromServer.getParameter("MessageType").equals("ServerHello")) {
+            X509Certificate clientCert = decodeCertificate(fromServer.getParameter("Certificate"));
+
+            verifyCertificate(getCertificate("C:/Users/azadm/IdeaProjects/VPN_Project/ca.pem"), clientCert);
+        }else{
+            System.out.println("error");
+        }
         socket.close();
 
         /*
          * Fake the handshake result with static parameters.
          */
 
-        /* This is to where the ForwardClient should connect. 
+        /* This is to where the ForwardClient should connect.
          * The ForwardServer creates a socket
          * dynamically and communicates the address (hostname and port number)
          * to ForwardClient during the handshake (ServerHost, ServerPort parameters).
-         * Here, we use a static address instead. 
+         * Here, we use a static address instead.
          */
         serverHost = Handshake.serverHost;
-        serverPort = Handshake.serverPort;        
+        serverPort = Handshake.serverPort;
     }
+
 
     /*
      * Let user know that we are waiting
@@ -66,15 +136,15 @@ public class ForwardClient
     private static void tellUser(ServerSocket listensocket) throws UnknownHostException {
         System.out.println("Client forwarder to target " + arguments.get("targethost") + ":" + arguments.get("targetport"));
         System.out.println("Waiting for incoming connections at " +
-                           InetAddress.getLocalHost().getHostAddress() + ":" + listensocket.getLocalPort());
+                InetAddress.getLocalHost().getHostAddress() + ":" + listensocket.getLocalPort());
     }
-        
+
     /*
      * Set up client forwarder.
      * Run handshake negotiation, then set up a listening socket and wait for user.
      * When user has connected, start port forwarder thread.
      */
-    static public void startForwardClient() throws IOException {
+    static public void startForwardClient() throws IOException, CertificateException, InvalidKeyException, NoSuchAlgorithmException, NoSuchProviderException, SignatureException {
 
         doHandshake();
 
@@ -82,24 +152,23 @@ public class ForwardClient
 
         ForwardServerClientThread forwardThread;
         ServerSocket listensocket;
-        
+
         try {
             /* Create a new socket. This is to where the user should connect.
              * ForwardClient sets up port forwarding between this socket
              * and the ServerHost/ServerPort learned from the handshake */
             listensocket = new ServerSocket();
             /* Let the system pick a port number */
-            listensocket.bind(null); 
-            /* Tell the user, so the user knows where to connect */ 
+            listensocket.bind(null);
+            /* Tell the user, so the user knows where to connect */
             tellUser(listensocket);
 
             Socket clientSocket = listensocket.accept();
             String clientHostPort = clientSocket.getInetAddress().getHostAddress() + ":" + clientSocket.getPort();
             log("Accepted client from " + clientHostPort);
-            
             forwardThread = new ForwardServerClientThread(clientSocket, serverHost, serverPort);
             forwardThread.start();
-            
+
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println(e);
@@ -111,32 +180,30 @@ public class ForwardClient
      * Prints given log message on the standart output if logging is enabled,
      * otherwise ignores it
      */
-    public static void log(String aMessage)
-    {
+    public static void log(String aMessage) {
         if (ENABLE_LOGGING)
-           System.out.println(aMessage);
+            System.out.println(aMessage);
     }
- 
+
     static void usage() {
         String indent = "";
         System.err.println(indent + "Usage: " + PROGRAMNAME + " options");
         System.err.println(indent + "Where options are:");
         indent += "    ";
         System.err.println(indent + "--targethost=<hostname>");
-        System.err.println(indent + "--targetport=<portnumber>");        
+        System.err.println(indent + "--targetport=<portnumber>");
         System.err.println(indent + "--handshakehost=<hostname>");
-        System.err.println(indent + "--handshakeport=<portnumber>");        
+        System.err.println(indent + "--handshakeport=<portnumber>");
         System.err.println(indent + "--usercert=<filename>");
         System.err.println(indent + "--cacert=<filename>");
-        System.err.println(indent + "--key=<filename>");                
+        System.err.println(indent + "--key=<filename>");
     }
-    
+
     /**
      * Program entry point. Reads arguments and run
      * the forward server
      */
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws NoSuchProviderException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
         try {
             arguments = new Arguments();
             arguments.setDefault("handshakeport", Integer.toString(DEFAULTSERVERPORT));
@@ -145,15 +212,17 @@ public class ForwardClient
             if (arguments.get("targetport") == null || arguments.get("targethost") == null) {
                 throw new IllegalArgumentException("Target not specified");
             }
-        } catch(IllegalArgumentException ex) {
+        } catch (IllegalArgumentException ex) {
             System.out.println(ex);
             usage();
             System.exit(1);
         }
         try {
             startForwardClient();
-        } catch(IOException e) {
-           e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
         }
     }
 }
--- VPN/ForwardServer.java
+++ VPN/ForwardServer.java
@@ -1,10 +1,50 @@
 /**
  * Port forwarding server. Forward data
- * between two TCP ports. Based on Nakov TCP Socket Forward Server 
+ * between two TCP ports. Based on Nakov TCP Socket Forward Server
  * and adapted for IK2206.
- *
+ * <p>
  * Original copyright notice below.
  * (c) 2018 Peter Sjodin, KTH
+ * <p>
+ * Nakov TCP Socket Forward Server - freeware
+ * Version 1.0 - March, 2002
+ * (c) 2001 by Svetlin Nakov - http://www.nakov.com
+ * <p>
+ * Nakov TCP Socket Forward Server - freeware
+ * Version 1.0 - March, 2002
+ * (c) 2001 by Svetlin Nakov - http://www.nakov.com
+ * <p>
+ * Nakov TCP Socket Forward Server - freeware
+ * Version 1.0 - March, 2002
+ * (c) 2001 by Svetlin Nakov - http://www.nakov.com
+ * <p>
+ * Nakov TCP Socket Forward Server - freeware
+ * Version 1.0 - March, 2002
+ * (c) 2001 by Svetlin Nakov - http://www.nakov.com
+ * <p>
+ * Nakov TCP Socket Forward Server - freeware
+ * Version 1.0 - March, 2002
+ * (c) 2001 by Svetlin Nakov - http://www.nakov.com
+ * <p>
+ * Nakov TCP Socket Forward Server - freeware
+ * Version 1.0 - March, 2002
+ * (c) 2001 by Svetlin Nakov - http://www.nakov.com
+ * <p>
+ * Nakov TCP Socket Forward Server - freeware
+ * Version 1.0 - March, 2002
+ * (c) 2001 by Svetlin Nakov - http://www.nakov.com
+ * <p>
+ * Nakov TCP Socket Forward Server - freeware
+ * Version 1.0 - March, 2002
+ * (c) 2001 by Svetlin Nakov - http://www.nakov.com
+ * <p>
+ * Nakov TCP Socket Forward Server - freeware
+ * Version 1.0 - March, 2002
+ * (c) 2001 by Svetlin Nakov - http://www.nakov.com
+ * <p>
+ * Nakov TCP Socket Forward Server - freeware
+ * Version 1.0 - March, 2002
+ * (c) 2001 by Svetlin Nakov - http://www.nakov.com
  */
 
 /**
@@ -12,58 +52,109 @@
  * Version 1.0 - March, 2002
  * (c) 2001 by Svetlin Nakov - http://www.nakov.com
  */
- 
-import java.lang.AssertionError;
-import java.lang.Integer;
-import java.util.ArrayList;
+
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.UnknownHostException;
-import java.io.IOException;
-import java.io.FileInputStream;
-import java.util.Properties;
-import java.util.StringTokenizer;
- 
-public class ForwardServer
-{
+import java.security.*;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.Base64;
+
+public class ForwardServer {
     private static final boolean ENABLE_LOGGING = true;
     public static final int DEFAULTSERVERPORT = 2206;
     public static final String DEFAULTSERVERHOST = "localhost";
     public static final String PROGRAMNAME = "ForwardServer";
     private static Arguments arguments;
 
-
     private ServerSocket handshakeSocket;
-    
+
     private ServerSocket listenSocket;
     private String targetHost;
     private int targetPort;
-    
+
+
     /**
      * Do handshake negotiation with client to authenticate, learn 
      * target host/port, etc.
      */
-    private void doHandshake() throws UnknownHostException, IOException, Exception {
+
+    public X509Certificate generateCertificate(byte[] preCert) throws CertificateException {
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(preCert);
+        CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+        return (X509Certificate) certificateFactory.generateCertificate(inputStream);
+    }
+
+    private X509Certificate decodeCertificate(String encodedCert) throws CertificateException {
+        byte[] decodedCert = Base64.getDecoder().decode(encodedCert.getBytes());
+        return generateCertificate(decodedCert);
+    }
+
+    private void verifyCertificate(X509Certificate caCert, X509Certificate clientCert) throws NoSuchProviderException, CertificateException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+        PublicKey publicKey = caCert.getPublicKey();
+        clientCert.verify(publicKey);
+    }
+
+    private static X509Certificate getCertificate(String input) throws IOException, CertificateException {
+        CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+        FileInputStream certInput = new FileInputStream(input);
+        X509Certificate certificate = (X509Certificate) certificateFactory.generateCertificate(certInput);
+        certInput.close();
+        return certificate;
+    }
+
+    private static String encodeCertificate(X509Certificate certificate) throws CertificateEncodingException {
+        //certificate.getEncoded();
+        byte[] encodedCert = Base64.getEncoder().encode(certificate.getEncoded());
+        return new String(encodedCert);
+    }
+
+    private void doHandshake() throws Exception {
 
         Socket clientSocket = handshakeSocket.accept();
+        System.out.println(clientSocket);
         String clientHostPort = clientSocket.getInetAddress().getHostAddress() + ":" + clientSocket.getPort();
+        System.out.println(clientHostPort);
         Logger.log("Incoming handshake connection from " + clientHostPort);
 
         /* This is where the handshake should take place */
-        
+        HandshakeMessage fromClient = new HandshakeMessage();
+        HandshakeMessage toClient = new HandshakeMessage();
+
+        fromClient.recv(clientSocket);
+        if (fromClient.getParameter("MessageType").equals("ClientHello")) {
+            X509Certificate clientCert = decodeCertificate(fromClient.getParameter("Certificate"));
+            //verifyCertificate(getCertificate((arguments.get("cacert"))), clientCert);
+            verifyCertificate(getCertificate("C:/Users/azadm/IdeaProjects/VPN_Project/ca.pem"), clientCert);
+
+            // This is supposed to be the ServerCertificate, but leave it as is for now
+            X509Certificate cert = getCertificate("C:/Users/azadm/IdeaProjects/VPN_Project/client.pem");
+
+            // send "userCert", but call it serverCert
+
+            toClient.putParameter("MessageType", "ServerHello");
+            toClient.putParameter("Certificate", encodeCertificate(cert));
+            toClient.send(clientSocket);
+        }
+
+
         clientSocket.close();
 
         /*
-         * Fake the handshake result with static parameters. 
+         * Fake the handshake result with static parameters.
          */
 
-        /* listenSocket is a new socket where the ForwardServer waits for the 
+        /* listenSocket is a new socket where the ForwardServer waits for the
          * client to connect. The ForwardServer creates this socket and communicates
-         * the socket's address to the ForwardClient during the handshake, so that the 
+         * the socket's address to the ForwardClient during the handshake, so that the
          * ForwardClient knows to where it should connect (ServerHost/ServerPort parameters).
-         * Here, we use a static address instead (serverHost/serverPort). 
+         * Here, we use a static address instead (serverHost/serverPort).
          * (This may give "Address already in use" errors, but that's OK for now.)
          */
         listenSocket = new ServerSocket();
@@ -73,7 +164,7 @@ public class ForwardServer
          * between the listensocket (ie., ServerHost/ServerPort) and the target.
          */
         targetHost = Handshake.targetHost;
-        targetPort = Handshake.targetPort;        
+        targetPort = Handshake.targetPort;
     }
 
     /**
@@ -81,74 +172,71 @@ public class ForwardServer
      */
     public void startForwardServer()
     //throws IOException
-        throws Exception
-    {
- 
+            throws Exception {
+
         // Bind server on given TCP port
         int port = Integer.parseInt(arguments.get("handshakeport"));
         try {
             handshakeSocket = new ServerSocket(port);
         } catch (IOException ioe) {
-           throw new IOException("Unable to bind to port " + port);
+            throw new IOException("Unable to bind to port " + port);
         }
 
         log("Nakov Forward Server started on TCP port " + port);
- 
+
         // Accept client connections and process them until stopped
-        while(true) {
+        while (true) {
             ForwardServerClientThread forwardThread;
-           try {
+            try {
+                System.out.println("Im here");
+                doHandshake();
 
-               doHandshake();
-
-               forwardThread = new ForwardServerClientThread(this.listenSocket, this.targetHost, this.targetPort);
-               forwardThread.start();
-           } catch (IOException e) {
-               throw e;
-           }
+                forwardThread = new ForwardServerClientThread(this.listenSocket, this.targetHost, this.targetPort);
+                forwardThread.start();
+            } catch (IOException e) {
+                throw e;
+            }
         }
     }
- 
+
     /**
      * Prints given log message on the standart output if logging is enabled,
      * otherwise ignores it
      */
-    public void log(String aMessage)
-    {
+    public void log(String aMessage) {
         if (ENABLE_LOGGING)
-           System.out.println(aMessage);
+            System.out.println(aMessage);
     }
- 
+
     static void usage() {
         String indent = "";
         System.err.println(indent + "Usage: " + PROGRAMNAME + " options");
         System.err.println(indent + "Where options are:");
         indent += "    ";
-        System.err.println(indent + "--serverhost=<hostname>");
-        System.err.println(indent + "--serverport=<portnumber>");        
+        System.err.println(indent + "--handshakehost=<hostname>");
+        System.err.println(indent + "--handshakeport=<portnumber>");
         System.err.println(indent + "--usercert=<filename>");
         System.err.println(indent + "--cacert=<filename>");
-        System.err.println(indent + "--key=<filename>");                
+        System.err.println(indent + "--key=<filename>");
     }
-    
+
     /**
      * Program entry point. Reads settings, starts check-alive thread and
      * the forward server
      */
     public static void main(String[] args)
-        throws Exception
-    {
+            throws Exception {
         arguments = new Arguments();
         arguments.setDefault("handshakeport", Integer.toString(DEFAULTSERVERPORT));
         arguments.setDefault("handshakehost", DEFAULTSERVERHOST);
         arguments.loadArguments(args);
-        
+
         ForwardServer srv = new ForwardServer();
         try {
-           srv.startForwardServer();
+            srv.startForwardServer();
         } catch (Exception e) {
-           e.printStackTrace();
+            e.printStackTrace();
         }
     }
- 
+
 }
